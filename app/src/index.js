5389156bdaec20062c6efc41c16123e10142b4ad
mbellemo <Cazzo.82>
2022-08-19 11:37:30 +0200

    Implement lookUp function in the FE

--- app/src/index.js
+++ app/src/index.js
@@ -41,7 +41,10 @@ const App = {
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const lookid = document.getElementById("lookid").value;
+    const starName = await lookUptokenIdToStarInfo(lookid).call({from: this.account});
+    App.setStatus("Star Id is " + lookid + ", Star Name is " + starName + ".");
   }
 
 };
