b674f81ee0f89fc94e1e9bca42896e4b31a5850b
mbellemo <Cazzo.82>
2022-08-19 09:01:00 +0200

    Add transferStar function and related test.

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -115,4 +115,11 @@ it('lets a user transfer a star', async() => {
     // 1. create a Star with different tokenId
     // 2. use the transferStar function implemented in the Smart Contract
     // 3. Verify the star owner changed.
+    let instance = await StarNotary.deployed();
+    let user1 = accounts[1];
+    let user2 = accounts[2];
+    let starId = 9;
+    await instance.createStar("Awesome Star", starId, {from: user1});
+    await instance.transferStar(user2, starId, {from: user1});
+    assert.equal(await instance.ownerOf(starId), user2);
 });
